b7b70684a9cc8895a2315e920d7b8947a41c5024
CtripleU <cuoyemike@gmail.com>
2022-07-22 00:25:30 +0200

    task 8 fixes

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
-
-def safe_function(fct, *args):
-    import sys
-    try:
-        var = fct(*args)
-        return var
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-        return None
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result += a ** b / i
+        except Exception:
+            result = b + a
+            break
+    return result
