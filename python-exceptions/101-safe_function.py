e59446f49380a2938289bddbb4398aefb8ebfdff
CtripleU <cuoyemike@gmail.com>
2022-07-22 00:23:13 +0200

    task 8 fixes

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_function(fct, *args):
     import sys
     try:
@@ -8,5 +9,3 @@ def safe_function(fct, *args):
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return None
-
-
