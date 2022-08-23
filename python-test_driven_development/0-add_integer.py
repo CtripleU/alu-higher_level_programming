2b36625245d37d2607338242ba42ecc65facc670
Cumi Oyemike <cuoyemike@gmail.com>
2022-08-23 21:27:41 +0200

    task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+def add_integer(a=0, b=98):
+    """Adds two numbers and returns the sum"""
+
+    if type(a) == float or type(b) == float:
+        a = int(a)
+        b = int(b)
+
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b) != int:
+        raise TypeError("b must be an integer")
+
+    return a + b
