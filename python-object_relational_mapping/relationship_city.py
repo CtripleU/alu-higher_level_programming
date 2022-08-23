212869e1192061fd46e1b7f27dea7d62a2a380ce
Cumi Oyemike <cuoyemike@gmail.com>
2022-08-24 01:18:27 +0200

    fixes

--- python-object_relational_mapping/relationship_city.py
+++ python-object_relational_mapping/relationship_city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""model city module documentation"""
-
+"""
+model city module documentation
+"""
 
 from model_state import Base, State
 from sqlalchemy import Column, Integer, ForeignKey, String
--- python-object_relational_mapping/relationship_state.py
+++ python-object_relational_mapping/relationship_state.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 """ 
-model state 
-database declaration documentation
+Lists all State objects that contain the letter a
+from the database
+Usage: ./9-model_state_filter_a.py <mysql username> /
+                                   <mysql password> /
+                                   <database name>
 """
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
 
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-
-Base = declarative_base()
-
-
-class State(Base):
-    """ 
-    State class table 
-    """
-    # declare table name
-    __tablename__ = 'states'
-    # define colum names and characteristics
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
