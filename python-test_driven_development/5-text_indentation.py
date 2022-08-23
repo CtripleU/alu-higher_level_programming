a03ec586d35ff6810c8be97b9c13f6b03c9fffc0
Cumi Oyemike <cuoyemike@gmail.com>
2022-08-23 23:01:17 +0200

    more fixes

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,25 +2,20 @@
 """Documentation"""
 
 
+
 def text_indentation(text):
     """
     Prints a text with 2 new lines after
     each of these characters: ., ? and :
     """
-    if not isinstance(text, str):
+
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
-    i = 0
-    while i < len(text) and text[i] == ' ':
-        i += 1
-
-    while i < len(text):
-        print(text[i], end="")
-        if text[i] == "\n" or text[i] in ".?:":
-            if text[i] in ".?:":
-                print("\n")
-            i += 1
-            while i < len(text) and text[i] == ' ':
-                i += 1
-            continue
-        i += 1
+    line = ""
+    for c in text:
+        line += c
+        if c in ['.', '?', ':']:
+            print((line + '\n').lstrip(' '))
+            line = ""
+    print(line.lstrip(' '), end="")
