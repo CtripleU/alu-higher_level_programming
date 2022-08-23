9fe10ca82a23969350ceb09874d6d86f9b290a38
Cumi Oyemike <cuoyemike@gmail.com>
2022-08-23 23:38:37 +0200

    fixes

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -4,10 +4,10 @@
 
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
     """
     function that multiplies 2 matrices
     by using the module NumPy
     """
     return np.matmul(m_a, m_b)
-    
