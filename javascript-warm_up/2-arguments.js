96c17b37bfd56007e61f165dcbd4b0505d53a14d
CtripleU <cuoyemike@gmail.com>
2022-10-07 20:19:34 +0200

    removed function

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
-function myFunction(a, b, c) {
+// function myFunction (a, b, c) {
 
-  if (arguments.length === 0) {
-    console.log('No argument');
-  } else if (arguments.length === 1) {
-      console.log('Argument found');
-  } else {
-      console.log('Arguments found');
-  }
-
+if (arguments.length === 0) {
+console.log('No argument');
+} else if (arguments.length === 1) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
 }
 
-// myFunction();
+// }
+
+// myFunction (a, b, c);
 
 // myFunction(1);
 
