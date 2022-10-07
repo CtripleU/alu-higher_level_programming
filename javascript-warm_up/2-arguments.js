288410f4f9b47118f9035c10cb6a536e44f86823
CtripleU <cuoyemike@gmail.com>
2022-10-07 20:25:20 +0200

    changed argument function

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,17 +1,25 @@
 #!/usr/bin/node
+if (process.argv.length === 0) {
+  console.log('No argument');
+} else if (process.argv.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
+      
 // function myFunction (a, b, c) {
 
-if (arguments.length === 0) {
-console.log('No argument');
-} else if (arguments.length === 1) {
-    console.log('Argument found');
-} else {
-    console.log('Arguments found');
-}
-
+//   if (arguments.length === 0) {
+//     console.log('No argument');
+//   } else if (arguments.length === 1) {
+//       console.log('Argument found');
+//   } else {
+//       console.log('Arguments found');
+//   }
+  
 // }
 
-// myFunction (a, b, c);
+// myFunction();
 
 // myFunction(1);
 
