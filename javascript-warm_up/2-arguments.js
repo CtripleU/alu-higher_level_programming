d5e537c4c27615fa18d2aee317a19d5cf96096f6
CtripleU <cuoyemike@gmail.com>
2022-10-07 20:13:38 +0200

    fixes

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,13 +1,18 @@
 #!/usr/bin/node
 function myFunction(a, b, c) {
 
-    if (arguments.length === 0) {
-        console.log('No argument');
-    } else if (arguments.length === 1) {
-        console.log('Argument found');
-    } else {
-        console.log('Arguments found');
-    }
+  if (arguments.length === 0) {
+    console.log('No argument');
+  } else if (arguments.length === 1) {
+      console.log('Argument found');
+  } else {
+      console.log('Arguments found');
+  }
+  
 }
 
+myFunction();
+
 myFunction(1);
+
+myFunction(1, 2);
