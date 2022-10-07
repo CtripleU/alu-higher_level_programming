3dca7c895578bb1e8c5ce58e46fbfaf82fe7d633
CtripleU <cuoyemike@gmail.com>
2022-10-07 21:09:44 +0200

    fixes

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (parseInt(process.argv[2]) === NaN) {
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
     console.log('Not a number');
 } else {
     console.log('My number: ' + parseInt(process.argv[2]));
