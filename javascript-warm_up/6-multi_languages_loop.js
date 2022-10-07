645d474861b609d0bb8c9b8a41265f1b9b80cd93
CtripleU <cuoyemike@gmail.com>
2022-10-07 21:43:58 +0200

    fixes

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-multiLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const multiLanguages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 for (i = 0 ; i < multiLanguages.length ; i++) {
   console.log(multiLanguages[i]);
