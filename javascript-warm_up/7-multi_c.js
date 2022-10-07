4de2e75102f55c4892aca360e16ef0ae51095c42
CtripleU <cuoyemike@gmail.com>
2022-10-07 22:04:20 +0200

    changed algorithm

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,6 +1,18 @@
 #!/usr/bin/node
+
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Missing number of occurences');
 } else {
-  console.log("C is fun\n".repeat(process.argv[2]));
+  const c = parseInt(process.argv[2]);
+  let i = 0;
+  for (i = 0; i < c; i++) {
+    console.log("C is fun");
+  }
 }
+
+
+// if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+//   console.log('Missing number of occurences');
+// } else {
+//   console.log("C is fun\n".repeat(process.argv[2]));
+// }
