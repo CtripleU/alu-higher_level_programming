ac9b978258fe74759f3d505f0560d13f0e95dc6b
CtripleU <cuoyemike@gmail.com>
2022-10-07 20:08:54 +0200

    task2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function myFunction(a, b, c) {
+
+    if (arguments.length === 0) {
+        console.log('No argument');
+    } else if (arguments.length === 1) {
+        console.log('Argument found');
+    } else {
+        console.log('Arguments found');
+    }
+}
+
+myFunction(1);
