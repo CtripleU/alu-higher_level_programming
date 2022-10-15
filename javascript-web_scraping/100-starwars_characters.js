65be19f50ceb9f628a9d334d153a743241370319
CtripleU <cuoyemike@gmail.com>
2022-10-15 05:48:04 +0200

    changes

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
-const process = require('process');
+const movieId = process.argv.slice(2)[0];
 const request = require('request');
-const movieId = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-request(url, 'utf-8', async (err, resp, body) => {
-  if (!err) {
-    const movie = JSON.parse(body);
-    const chars = movie.characters;
-    const newPromise = (url) => {
-      return new Promise(function (resolve, reject) {
-        request(url, 'utf-8', (err, resp, body) => {
-          if (err) reject(err);
-          else resolve(body);
-        });
+
+const filmsUrl = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(filmsUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const parseData = JSON.parse(body);
+    const characters = parseData.characters;
+    characters.forEach(charUrl => {
+      request(charUrl, (err, resp, bdy) => {
+        if (err) console.log(err);
+        else {
+          const parseCharData = JSON.parse(bdy);
+          console.log(parseCharData.name);
+        }
       });
-    };
-    for (const each in chars) {
-      const actor = await newPromise(chars[each]);
-      const charList = JSON.parse(actor);
-      console.log(charList.name);
-    }
+    });
   }
 });
