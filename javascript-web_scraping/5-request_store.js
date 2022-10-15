b178120ed3547d0522c8e218534f3b0b75db88ac
CtripleU <cuoyemike@gmail.com>
2022-10-15 05:36:30 +0200

    fixes

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-
-const request = require('request');
+const req = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
-  }
+const file = process.argv[3];
+req.get(url, async (err, res) => {
+  err
+    ? console.log(err)
+    : await fs.writeFile(file, res.body, (err) => {
+      if (err) console.log(err);
+    });
 });
